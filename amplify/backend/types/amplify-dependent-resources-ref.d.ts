e4de8cb951d231029df3d2b13c8f82a8b320f23f
“Parthasarathy <pssastry@amazon.com>
2023-01-10 16:39:01 -0500

    test

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactfiless3e1e15360": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "reactfiless3": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Storage } from "aws-amplify";
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [singleImage, setSingleImage] = useState(null);
+  const [multipleFiles, setMultipleFiles] = useState([]);
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = async () => {
+    let imageKeys = await Storage.list("folder/"); // You can add /filder inside
+    // console.log("imageKeys: ", imageKeys.results);
+    let imageKeysUrl = await Promise.all(
+      imageKeys.results.map(async (k) => {
+        const signedUrl = await Storage.get(k.key);
+        return signedUrl;
+      })
+    );
+    // console.log("imageKeysUrl: ", imageKeysUrl);
+    setImages(imageKeysUrl);
+
+    let Key = "folder/DL.jpeg";
+    let keyUrl = await Storage.get(Key);
+    // console.log(keyUrl);
+    setSingleImage(keyUrl);
+  };
+
+  const onChange = (e) => {
+    const file = e.target.files[0];
+    const files = [...e.target.files];
+    const folder = "folder/";
+    // const fileName = folder + file.name;
+    // const result = await Storage.put(fileName, file);
+
+    const res = files.map(async (file) => {
+      const fileName = folder + file.name;
+      const result = await Storage.put(fileName, file);
+      console.log(result);
+      fetchImages();
+    });
+    // console.log("result: ", result);
+    console.log(files);
+
+    console.log("multiple files: ", multipleFiles);
+    // fetchImages();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Storage Example (S3)</div>
+      <div>
+        <input
+          type="file"
+          accept=".png, .jpeg, .jpg"
+          multiple
+          onChange={onChange}
+        />
+      </div>
+      {/* <div>
+        <p>Single file</p>
+        <img
+          src={singleImage}
+          key={singleImage}
+          style={{ width: 300, height: 200, marginBottom: 10 }}
+        />
+      </div> */}
+      {images.map((image) => {
+        return (
+          <img
+            src={image}
+            key={image}
+            style={{ width: 300, height: 200, marginBottom: 10 }}
+          />
+        );
+      })}
+    </>
   );
-}
+};
 
 export default App;
